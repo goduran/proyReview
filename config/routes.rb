dad93877b943129c8e10c125b95d47ce7a424e81
Gabriel <goduran@gmail.com>
2017-11-17 00:34:01 -0600

    stylin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :reviews
-  resources :entries
+  resources :entries do
+    resources :reviews
+  end
   root 'entries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
